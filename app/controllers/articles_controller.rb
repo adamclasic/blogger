3489b54bba28892927acb269e5e622d6d7197c07
adam <aabou9@gmail.com>
2020-05-29 19:28:52 +0100

    add flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,12 +16,13 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
-
+    flash.notice = "Article '#{@article.title}' created!"
     redirect_to article_path(@article)
   end
 
   def destroy
     Article.find(params[:id]).destroy
+    flash.notice = "Article removed!"
     redirect_to articles_path
 
   end
@@ -34,6 +35,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' Updated!"
     redirect_to article_path(@article)
   end
 end
