eaa12d0c31e2b6606b06ddb5936104841cd0b3c3
adam <aabou9@gmail.com>
2020-05-29 16:27:02 +0100

    add parmas delete action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,15 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
-    
+
     redirect_to article_path(@article)
+  end
+
+  def destroy
+    # @article = Article.destroy(params[:id])
+    # @article.
+    Article.find(params[:id]).destroy
+    redirect_to articles_path
+
   end
 end
