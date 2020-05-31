b46d04c5f8c478640e7ab8404b66ac268c758d5b
adam <aabou9@gmail.com>
2020-05-31 11:29:19 +0100

    add tag list selector

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,14 @@ class Article < ApplicationRecord
     has_many :comments
     has_many :taggings
     has_many :tags, through: :taggings
+
+    def tag_list
+        self.tags.collect do |tag|
+            tag.name
+        end.join(", ")
+    end
+
+    def tag_list=(tags_string)
+
+    end
 end
