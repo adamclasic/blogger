13ba358c7567bef74aa135680c6619c5dc51cb30
adam <aabou9@gmail.com>
2020-05-31 12:01:19 +0100

    add taglist to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,6 +10,8 @@ class Article < ApplicationRecord
     end
 
     def tag_list=(tags_string)
-
+        tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+        new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+        self.tags = new_or_found_tags
     end
 end
