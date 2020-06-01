8e47a2cfcab82728c8b967faf47b575023ae6718
adam <aabou9@gmail.com>
2020-06-01 14:36:47 +0100

    add login to routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,10 @@ Rails.application.routes.draw do
   end
   resources :tags
   resources :authors
+
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
   
 end
