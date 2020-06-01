901375fbe1b780ad3874727795cb48621194779e
adam <aabou9@gmail.com>
2020-06-01 10:11:26 +0100

    add username to migration

--- db/migrate/20200601085956_sorcery_core.rb
+++ db/migrate/20200601085956_sorcery_core.rb
@@ -1,9 +1,10 @@
 class SorceryCore < ActiveRecord::Migration[5.2]
   def change
     create_table :authors do |t|
-      t.string :email,            null: false
-      t.string :crypted_password
-      t.string :salt
+      t.string :username,         :null => false
+      t.string :email,            :null => false
+      t.string :crypted_password, :null => false
+      t.string :salt,             :null => false
 
       t.timestamps                null: false
     end
